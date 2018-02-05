aeb77191c944113ab261d451a037ad7b546c053a
陈俊 <chenjun835@163.com>
2018-02-05 17:41:11 +0800

    Initial commit

---
+++ src/main/java/net/zriot/ebike/EbikeServerApplication.java
@@ -0,0 +1,12 @@
+package net.zriot.ebike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbikeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbikeServerApplication.class, args);
+	}
+}
