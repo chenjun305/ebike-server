2c9e0c355fe8bd410affdccbdba742073b9804af
陈俊 <chenjun835@163.com>
2018-03-18 14:58:04 +0800

    config 3 modules

--- ebike-server-admin/src/main/java/net/zriot/ebike/EbikeServerAdmin.java
+++ ebike-server-admin/src/main/java/net/zriot/ebike/EbikeServerAdmin.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EbikeServerApplication {
+public class EbikeServerAdmin {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EbikeServerApplication.class, args);
+		SpringApplication.run(EbikeServerAdmin.class, args);
 	}
 }
