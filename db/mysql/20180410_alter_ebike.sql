489b9d1e1aec5606c0596d023f971b3b8b1f5bf6
陈俊 <chenjun835@163.com>
2018-04-12 15:16:34 +0800

    change table

--- db/mysql/20180410_alter_ebike.sql
+++
@@ -1,3 +0,0 @@
-alter table `ebike` drop column `shop_id`;
-alter table `ebike` add column month_num tinyint unsigned default '0' comment '月换电次数' after is_membership;
-alter table `payment_order` add column month_num tinyint comment '月换电次数' after shop_id;
---
+++ db/mysql/20180412_alter_table.sql
@@ -0,0 +1,5 @@
+alter table `ebike` drop column `month_num`;
+alter table `ebike` add column month_total tinyint unsigned default '0' comment '月换电套餐总次数' after is_membership;
+alter table `ebike` add column month_left tinyint unsigned default '0' comment '月换电套餐剩余次数' after is_membership;
+alter table `payment_order` add column product_id INT(11) UNSIGNED  comment '产品ID' after ebike_sn;
+update payment_order set product_id=1 where type != 2;
