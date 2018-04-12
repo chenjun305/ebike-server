eb813c1f17b2be6c1b9705172a2b07398c8a3963
陈俊 <chenjun835@163.com>
2018-04-12 15:54:04 +0800

    fix sql

--- db/mysql/20180412_alter_table.sql
+++ db/mysql/20180412_alter_table.sql
@@ -2,4 +2,4 @@ alter table `ebike` drop column `month_num`;
 alter table `ebike` add column month_total tinyint unsigned default '0' comment '月换电套餐总次数' after is_membership;
 alter table `ebike` add column month_left tinyint unsigned default '0' comment '月换电套餐剩余次数' after is_membership;
 alter table `payment_order` add column product_id INT(11) UNSIGNED  comment '产品ID' after ebike_sn;
-update payment_order set product_id=1 where type != 2;
+update payment_order set product_id=1 where ebike_sn IS NOT NULL;
