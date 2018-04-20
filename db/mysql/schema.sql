61da886416c52924ed11e6fc342bfd38841f97e3
陈俊 <chenjun835@163.com>
2018-04-20 13:52:22 +0800

    create table user_role

--- db/mysql/schema.sql
+++ db/mysql/schema.sql
@@ -20,27 +20,21 @@ CREATE TABLE `user` (
   UNIQUE KEY `uid_idx` (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
 
-CREATE TABLE `staff` (
+CREATE TABLE `user_role` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增唯一id',
   `uid` varchar(32) NOT NULL COMMENT 'ID',
   `tel` varchar(12) NOT NULL COMMENT '手机号',
-  `avatar` varchar(128) DEFAULT NULL COMMENT '头像',
-  `real_name` varchar(100) DEFAULT NULL COMMENT '真实姓名',
-  `gender` tinyint(2) DEFAULT '0' COMMENT '性别  1男  2女 0未知',
-  `id_card_num` VARCHAR(64) DEFAULT NULL COMMENT 'ID Card Number',
-  `id_card_pics` VARCHAR(256) DEFAULT NULL COMMENT 'ID Card 照片',
-  `shop_id` int(11) unsigned DEFAULT '0' COMMENT '所属商店ID',
-  `role` tinyint(2) DEFAULT '0' COMMENT '用户角色:0未知 1店长 2店员',
-  `staff_num` VARCHAR(32) DEFAULT NULL COMMENT '员工编号',
-  `address` varchar(128) DEFAULT NULL COMMENT '用户地址',
+  `role` tinyint(2) NOT NULL COMMENT '用户角色: 0未知 1店长 2店员 3运营管理',
+  `staff_num` VARCHAR(32) COMMENT '员工编号',
+  `shop_id` int(11) unsigned COMMENT '所属商店ID',
   `status` tinyint(1) DEFAULT '1' COMMENT '用户状态:0禁用 1正常',
   `create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `tel` (`tel`) USING BTREE,
-  UNIQUE KEY `uid_idx` (`uid`),
+  KEY `tel_idx` (`tel`),
+  KEY `uid_idx` (`uid`),
   KEY `shop_idx` (`shop_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='工作人员表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户角色表';
 
 CREATE TABLE `shop` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增唯一id',
