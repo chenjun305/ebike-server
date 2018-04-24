f97ae4025223187aaa02c414c9367238d2ab307b
陈俊 <chenjun835@163.com>
2018-04-24 15:23:39 +0800

    restart-ebike.sh

---
+++ restart-ebike.sh
@@ -0,0 +1,7 @@
+./deploy-ebike.sh stop ebike-server-admin
+./deploy-ebike.sh stop ebike-server-user-app
+./deploy-ebike.sh stop ebike-server-shop-app
+
+./deploy-ebike.sh start ebike-server-admin
+./deploy-ebike.sh start ebike-server-user-app
+./deploy-ebike.sh start ebike-server-shop-app
