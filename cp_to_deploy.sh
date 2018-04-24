6d9ba788bab3a433189577e9452614321965160f
陈俊 <chenjun835@163.com>
2018-04-24 15:15:49 +0800

    add shell cp_to_deploy.sh

---
+++ cp_to_deploy.sh
@@ -0,0 +1,4 @@
+cp ebike-server-core/err_code.xml ~/Documents/
+cp ebike-server-user-app/target/ebike-server-user-app.jar ~/Documents/
+cp ebike-server-shop-app/target/ebike-server-shop-app.jar ~/Documents/
+cp ebike-server-admin/target/ebike-server-admin.jar ~/Documents/
