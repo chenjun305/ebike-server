d5d3f8ee7a0282967bc3603b928283a41f458b14
陈俊 <chenjun835@163.com>
2018-05-03 18:00:28 +0800

    change shell

--- restart-ebike.sh
+++ restart-ebike.sh
@@ -1,7 +1,9 @@
 ./deploy-ebike.sh stop ebike-server-admin
 ./deploy-ebike.sh stop ebike-server-user-app
 ./deploy-ebike.sh stop ebike-server-shop-app
+./deploy-ebike.sh stop ebike-server-task
 
 ./deploy-ebike.sh start ebike-server-admin
 ./deploy-ebike.sh start ebike-server-user-app
 ./deploy-ebike.sh start ebike-server-shop-app
+./deploy-ebike.sh start ebike-server-task
