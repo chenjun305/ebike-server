c5e4c6f378792757d89b759d16b4ce6ff0399098
陈俊 <chenjun835@163.com>
2018-05-11 17:03:37 +0800

    change shell

--- cp_to_deploy.sh
+++ cp_to_deploy.sh
@@ -2,4 +2,3 @@ cp ebike-server-core/err_code.xml ~/Documents/
 cp ebike-server-user-app/target/ebike-server-user-app.jar ~/Documents/
 cp ebike-server-shop-app/target/ebike-server-shop-app.jar ~/Documents/
 cp ebike-server-admin/target/ebike-server-admin.jar ~/Documents/
-cp ebike-server-task/target/ebike-server-task.jar ~/Documents/
