bddfefe4201ff0eb5ce46a0b59342c1e5bc8d6cf
陈俊 <chenjun835@163.com>
2018-09-18 15:41:36 +0800

    add scripts

--- cp_to_deploy.sh
+++ cp_to_deploy.sh
@@ -2,3 +2,4 @@ cp ebike-server-core/err_code.xml ~/Documents/
 cp ebike-server-user-app/target/ebike-server-user-app.jar ~/Documents/
 cp ebike-server-shop-app/target/ebike-server-shop-app.jar ~/Documents/
 cp ebike-server-admin/target/ebike-server-admin.jar ~/Documents/
+cp ebike-server-charger/target/ebike-server-charger.jar ~/Documents/
--- restart-ebike.sh
+++ restart-ebike.sh
@@ -1,7 +1,9 @@
 ./deploy-ebike.sh stop ebike-server-admin
 ./deploy-ebike.sh stop ebike-server-user-app
 ./deploy-ebike.sh stop ebike-server-shop-app
+./deploy-ebike.sh stop ebike-server-charger
 
 ./deploy-ebike.sh start ebike-server-admin
 ./deploy-ebike.sh start ebike-server-user-app
 ./deploy-ebike.sh start ebike-server-shop-app
+./deploy-ebike.sh start ebike-server-charger
